362efd3a52a684787723c77bddedfe39dfea339d
fangjunyu <fangjunyu.com@gmail.com>
2025-03-03 11:09:27 +0800

    Initial Commit

---
+++ Faock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Faock
+//
+//  Created by 方君宇 on 2025/3/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Faock/FaockApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaockApp.swift
+//  Faock
+//
+//  Created by 方君宇 on 2025/3/3.
+//
+
+import SwiftUI
+
+@main
+struct FaockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
